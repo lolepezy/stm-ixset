4e46b879a62753eef3b678004a417186898c9709
Mikhail Puzanov <mpuzanov@ripe.net>
2018-05-07 10:25:01 +0200

    Add map tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,29 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TupleSections #-}
+
+import Test.Tasty
+import Test.Tasty.QuickCheck as QC
+import qualified Test.Tasty.HUnit as HU
+import Test.QuickCheck.Monadic
+
+import Control.Monad.STM
+
+import Data.IxSet.STMTreeMap as M
+
+import Tests.STMTreeMapSpec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain $ testGroup "All tests" [qcStmMapProps]
