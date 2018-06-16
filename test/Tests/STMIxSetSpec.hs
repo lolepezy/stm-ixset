6ca1a3e4aed22da635f86a5d2b9e43125b006bfb
Mikhail Puzanov <mpuzanov@ripe.net>
2018-06-16 16:42:14 +0200

    Add realistic test.

--- test/Tests/STMIxSetSpec.hs
+++ test/Tests/STMIxSetSpec.hs
@@ -35,28 +35,8 @@ import GHC.Generics (Generic)
 qcStmIxsProps = testGroup "STM IxSet properties"
   [
     QC.testProperty
-      "Get after insert returns the same element"
-      prop_get_always_returns_inserted_element,
-
-    QC.testProperty
-      "Get after insert returns the same element unless it's removed explicitly"
-      prop_get_always_returns_inserted_element_except_for_removed_ones,
-
-    QC.testProperty
-      "Size of the map is always the number of unique elements"
-      prop_size_is_always_the_number_of_unique_elements,
-
-    QC.testProperty
-      "getLT should return keys smaller than the argument"
-      prop_getLT_returns_smaller_elements,
-
-    QC.testProperty
-      "getGT should return keys bigger than the argument"
-      prop_getGT_returns_smaller_elements,
-
-    QC.testProperty
-      "getBetween should return keys between the boundaries"
-      prop_getBetween_returns_proper_elements
+      "Get after insert returns the same elements"
+      prop_get_always_returns_inserted_elements
   ]
 
 
@@ -75,84 +55,31 @@ instance Arbitrary Index2 where
 instance Hashable Index1
 instance Hashable Index2
 
-mkIdxSet = atomically $ do
-  i1 <- idxFun (\((_, s, _) :: Entry) -> s)
-  i2 <- idxFun (\((i, _, _) :: Entry) -> i)
-  i3 <- idxFun (\((_, _, i) :: Entry) -> i)
-  return $ Ixs.new $ i3 :-: TNil
+field1 :: Entry -> Index1
+field1 (i, _, _) = i
+field2 :: Entry -> String
+field2 (_, s, _) = s
+field3 :: Entry -> Index2
+field3 (_, _, i) = i
+
+mkIdxSet = do
+  i1 <- idxFun field1
+  i2 <- idxFun field2
+  i3 <- idxFun field3
+  Ixs.new $ i1 :-: i2 :-: i3 :-: TNil
 
 
-prop_get_always_returns_inserted_element :: QC.Property
-prop_get_always_returns_inserted_element = monadicIO $ do
-  keys :: [Int] <- pick arbitrary
-  m <- run $ atomically Idx.new
-  run $ forM keys $ \k ->
-    atomically $ Idx.insert m k ("v" ++ show k)
-
-  values <- run $ forM keys $ \k ->
-    atomically $ Idx.get m k
-
-  assert $ [ "v" ++ show k | k <- keys] == catMaybes values
-
-
-prop_get_always_returns_inserted_element_except_for_removed_ones :: QC.Property
-prop_get_always_returns_inserted_element_except_for_removed_ones = monadicIO $ do
-  keys :: [Int] <- nub <$> pick arbitrary
-  m <- run $ atomically Idx.new
-  run $ forM keys $ \k ->
-    atomically $ Idx.insert m k ("v" ++ show k)
-
-  toBeRemoved <- nub <$> pick (sublistOf keys)
-  run $ forM toBeRemoved $ \k ->
-    atomically $ Idx.remove m k
-
-  values <- run $ forM keys $ \k ->
-    atomically $ Idx.get m k
-
-  assert $ [ "v" ++ show k | k <- keys \\ toBeRemoved] == catMaybes values
-
-
-prop_size_is_always_the_number_of_unique_elements :: QC.Property
-prop_size_is_always_the_number_of_unique_elements = monadicIO $ do
-  keys :: [Int] <- pick arbitrary
-  m <- run $ atomically Idx.new
-  run $ forM keys $ \k ->
-    atomically $ Idx.insert m k ("v" ++ show k)
-
-  s <- run $ atomically $ Idx.size m
-  assert $ s == length (nub keys)
-
-
-prop_getLT_returns_smaller_elements :: QC.Property
-prop_getLT_returns_smaller_elements = lowerGreaterTest (<) Idx.getLT
-
-prop_getGT_returns_smaller_elements :: QC.Property
-prop_getGT_returns_smaller_elements = lowerGreaterTest (>) Idx.getGT
-
-lowerGreaterTest :: (Int -> Int -> Bool) -> (TTree Int String -> Int -> STM [(Int, String)]) -> QC.Property
-lowerGreaterTest comparison extract = monadicIO $ do
-  keys :: [Int] <- pick arbitrary
-  m <- run $ atomically Idx.new
-  run $ forM keys $ \k ->
-    atomically $ Idx.insert m k ("v" ++ show k)
-
-  boundary <- pick (elements keys)
-  upper <- run $ atomically $ extract m boundary
-
-  let inRange = [ k | (k, v) <- upper, k `comparison` boundary ]
-  assert $ sort inRange == sort (map fst upper)
-
-
-prop_getBetween_returns_proper_elements :: QC.Property
-prop_getBetween_returns_proper_elements = monadicIO $ do
-  keys :: [Int] <- pick (suchThat arbitrary (not . null))
-  m <- run $ atomically Idx.new
-  run $ forM keys $ \k ->
-    atomically $ Idx.insert m k ("v" ++ show k)
-
-  lower   <- pick (elements keys)
-  greater <- pick (elements keys)
-  result  <- run $ atomically $ Idx.getBetween m lower greater
-
-  let inRange = [ k | (k, v) <- result, (k > lower) && (k < greater) ]
-  assert $ sort inRange == sort (map fst result)
+prop_get_always_returns_inserted_elements :: QC.Property
+prop_get_always_returns_inserted_elements = monadicIO $ do
+  ixSet <- run $ atomically mkIdxSet
+  entries :: [Entry] <- pick arbitrary
+  run $ atomically $ forM_ entries $ Ixs.insert ixSet
+  
+  compareExtractedSets ixSet entries field1
+  compareExtractedSets ixSet entries field2
+  compareExtractedSets ixSet entries field3
+  where
+    compareExtractedSets ixSet entries fieldF = do
+      i1Subset <- nub <$> pick (sublistOf entries)
+      i1Extracted <- run $ atomically $ forM i1Subset $ \e -> Ixs.get ixSet (fieldF e)
+      assert $ all (True==) [ all (\e -> fieldF e0 == fieldF e) extr | (e0, extr) <- zip i1Subset i1Extracted ]
